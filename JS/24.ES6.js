c15053162d8b71bd4761088c1fb0ca103a84b971
Hitesh Sinha <sinhahitesh0307@gmail.com>
2021-12-10 23:56:52 +0530

    Update 24.ES6.js

--- JS/24.ES6.js
+++ JS/24.ES6.js
@@ -43,3 +43,19 @@ let new_obj = new Student("Hitesh", "IX B", 15);
 console.log(new_obj);
 console.log(new_obj.Intro());
 
+// INHERITING THE CLASS INTO ANOTHER:
+class Inherited extends Student {
+          constructor (y_name, y_class, y_age, y_hobby, y_house){
+                    super(y_name, y_class, y_age);
+                    // Since we are inheriting  the properties of another class, we can copy their properties of contructor using this "super()"  and writing the corresponding values inside those brackets.
+                    // You can add new properties to them too, inheriting doesn't restricts anything....
+                    this.hobby = y_hobby;
+                    this.house = y_house;
+          };
+          // Still you can make many functions  whether static or not
+};
+
+let new_obj_2 = new Inherited("Shubham","IX C",15,"Cricket","Red");
+console.log(new_obj_2);
+console.log(new_obj_2.age);
+// This is just for showing that inheriting still doesn't makes any other difference in working of classes.
