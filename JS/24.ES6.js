94e128ad5039c3d93b6b9e94af949c2e8134545b
Hitesh Sinha <sinhahitesh0307@gmail.com>
2021-12-10 23:55:49 +0530

    Update 24.ES6.js

--- JS/24.ES6.js
+++ JS/24.ES6.js
@@ -15,3 +15,21 @@ body.appendChild(heading);
 body.appendChild(heading_2);
 body.appendChild(separator);
 head_element.appendChild(title);
+
+class Student {
+          constructor(x_name, x_class, x_age) {
+                    this.name = x_name,
+                    this.class = x_class,
+                    this.age = x_age
+          };
+          //  Functions inside a class:(Goes to  prototype)
+          Intro() {
+                    return `My name is ${this.name} and I am from class ${this.class}.`;
+          };
+          // Declare as many functions as you want inside one  class.....
+
+          //Statit functions which do not requires new object to be desplayed:
+          static add(a, b, c) {
+                    return a + b + c;
+          };
+}
